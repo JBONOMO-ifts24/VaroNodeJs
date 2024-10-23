caab5faae750318524c0c5e808255c9eb6ae3d8f
Juan <29502079@ifts24.com>
2024-10-23 17:41:57 -0300

    modificacion de cuadros

--- controllers/cuadros.controller.js
+++ controllers/cuadros.controller.js
@@ -7,114 +7,176 @@
 // ano_realizado
 // nombre_archivo
 
-
 const db = require("../db/db");
 
 //// METODO GET  /////
 
 // Para todos las peliculas
 const allCuadros = (req, res) => {
-    const sql = "SELECT * FROM cuadros";
-    db.query(sql, (error, rows) => {
-        if(error){
-            return res.status(500).json({error : "ERROR: Intente mas tarde por favor"});
-        }
-        res.json(rows);
-    }); 
+  const sql =
+    "SELECT cuadros_v2.idcuadros_v2, cuadros_v2.nombre_cuadro , pintores.nombre_pintor as pintor ,cuadros_v2.ano_realizado, cuadros_v2.nombre_archivo, cuadros_v2.ubicacion_orig  FROM cuadros_v2 INNER JOIN pintores ON cuadros_v2.pintor = pintores.idpintores";
+  db.query(sql, (error, rows) => {
+    if (error) {
+      return res
+        .status(500)
+        .json({ error: "ERROR: Intente mas tarde por favor" });
+    }
+    res.json(rows);
+  });
 };
 
 // Para un Cuadro
 const showCuadro = (req, res) => {
-    console.log(req.params);
-    const {idcuadro} = req.params;
-    console.log(idcuadro);
-    const sql = "SELECT * FROM cuadros WHERE idcuadros = ?";
-    db.query(sql,[idcuadro], (error, rows) => {
-        console.log(rows);
-        if(error){
-            return res.status(500).json({error : "ERROR: Intente más tarde por favor"});
-        }
-        if(rows.length == 0){
-            return res.status(404).send({error : "ERROR: No existe el mensaje buscado"});
-        };
-        res.json(rows[0]); 
-        // me muestra el elemento en la posicion cero si existe.
-    }); 
+  console.log(req.params);
+  const { idcuadro } = req.params;
+  console.log(idcuadro);
+  const sql =
+    "SELECT cuadros_v2.idcuadros_v2, cuadros_v2.nombre_cuadro , pintores.nombre_pintor as pintor ,cuadros_v2.ano_realizado, cuadros_v2.nombre_archivo, cuadros_v2.ubicacion_orig  FROM cuadros_v2 INNER JOIN pintores ON cuadros_v2.pintor = pintores.idpintores WHERE idcuadros_v2 = ?";
+  db.query(sql, [idcuadro], (error, rows) => {
+    console.log(rows);
+    if (error) {
+      return res
+        .status(500)
+        .json({ error: "ERROR: Intente más tarde por favor" });
+    }
+    if (rows.length == 0) {
+      return res
+        .status(404)
+        .send({ error: "ERROR: No existe el mensaje buscado" });
+    }
+    res.json(rows[0]);
+    // me muestra el elemento en la posicion cero si existe.
+  });
 };
 
 //// METODO POST  ////
 const storeCuadro = (req, res) => {
-    console.log(req.file);
-    console.log(req.body);
-    let nombre_archivo = "";
+  console.log(req.file);
+  console.log(req.body);
+  let nombre_archivo = "";
 
-    if(req.file){
-        nombre_archivo = req.file.filename;
-    };
+  if (req.file) {
+    nombre_archivo = req.file.filename;
+  }
 
-    const {nombre_cuadro, pintor, ano_realizado} = req.body;
+  const { nombre_cuadro, pintor, ano_realizado, ubicacion_orig } = req.body;
 
-    const sql = "INSERT INTO cuadros (nombre_cuadro, pintor, ano_realizado, nombre_archivo) VALUES (?,?,?,?)";
+  const sqla = "SELECT idpintores FROM pintores WHERE nombre_pintor = ?";
+  const sql =
+    "INSERT INTO cuadros_v2 (nombre_cuadro , pintor , ano_realizado, nombre_archivo, ubicacion_orig) VALUES (?,?,?,?,?)";
+  db.query(sqla, [pintor], (error, resul) => {
+    console.log(`nombre pintor: ${pintor}`);
+    console.log(resul[0]);
 
-    db.query(sql,[nombre_cuadro, pintor, ano_realizado, nombre_archivo], (error, result) => {
-        console.log(result);
-        if(error){
-            return res.status(500).json({error : "ERROR: Intente mas tarde por favor"});
+    if (error) {
+      return res
+        .status(500)
+        .json({ error: "ERROR: Intente más tarde por favor" });
+    }
+
+    try {
+      db.query(
+        sql,
+        [
+          nombre_cuadro,
+          resul[0].idpintores,
+          ano_realizado,
+          nombre_archivo,
+          ubicacion_orig,
+        ],
+        (error, result) => {
+          if (error) {
+            return res
+              .status(500)
+              .json({ error: "ERROR: Intente más tarde por favor" });
+          }
+          const mensaje = { ...req.body, id: result.insertId }; // ... reconstruir el objeto del body
+          res.status(201).json(mensaje); // muestra creado con éxito el elemento
         }
-        const cuadro = {...req.body, id: result.insertId}; // ... reconstruir el objeto del body
-        res.status(201).json(cuadro); // muestra creado con exito el elemento
-    });     
-
+      );
+    } catch (err) {
+      res.status(500).json({
+        error:
+          "ERROR: Ha ocurrido un problema inesperado. Intente más tarde por favor",
+      });
+    }
+  });
 };
 
 //// METODO PUT  ////
 const updateCuadro = (req, res) => {
-    console.log(req.params);
-    const {idcuadro} = req.params;
-    console.log(idcuadro);
-    const {nombre_cuadro, pintor, ano_realizado} = req.body;
-    console.log(idcuadro);
-    const sql ="UPDATE cuadros SET nombre_cuadro = ?, pintor = ?, ano_realizado = ? WHERE idcuadros = ?";
-    db.query(sql,[nombre_cuadro, pintor, ano_realizado, idcuadro], (error, result) => {
-        console.log(result);
-        if(error){
-            return res.status(500).json({error : "ERROR: Intente mas tarde por favor"});
+  console.log(req.params);
+  const { idcuadro } = req.params;
+  console.log(idcuadro);
+  const { nombre_cuadro, pintor, ano_realizado,ubicacion_orig } = req.body;
+  console.log(pintor);
+  const sql =
+    "UPDATE cuadros_v2 SET nombre_cuadro = ?, pintor =? ,ano_realizado =? ,ubicacion_orig = ? WHERE idcuadros_v2 = ?";
+  const sqla = "SELECT idpintores FROM pintores WHERE nombre_pintor = ?";
+  try {
+    db.query(sqla, [pintor], (error, resul) => {
+      console.log(resul[0].idpintores);
+      if (error) {
+        return res
+          .status(500)
+          .json({ error: "ERROR: Intente más tarde por favor" });
+      }
+
+      db.query(
+        sql,
+        [nombre_cuadro, resul[0].idpintores, ano_realizado, ubicacion_orig,idcuadro],
+        (error, result) => {
+          if (error) {
+            return res
+              .status(500)
+              .json({ error: "ERROR: Intente más tarde por favor" });
+          }
+          if (result.affectedRows == 0) {
+            return res
+              .status(404)
+              .send({ error: "ERROR: El cuadro a modificar no existe" });
+          }
+          const mensaje = { ...req.body, id: result.insertId }; // ... reconstruir el objeto del body
+          res.status(201).json(mensaje); // muestra creado con éxito el elemento
         }
-        if(result.affectedRows == 0){
-            return res.status(404).send({error : "ERROR: El cuadro a modificar no existe"});
-        };
-        
-        const cuadro = {...req.body, ...req.params}; // ... reconstruir el objeto del body
-
-        res.json(cuadro); // mostrar el elemento que existe
-    });     
+      );
+    });
+  } catch (err) {
+    res.status(500).json({
+      error:
+        "ERROR: Ha ocurrido un problema inesperado. Intente más tarde por favor",
+    });
+  }
 };
-
 
 //// METODO DELETE ////
 const destroyCuadro = (req, res) => {
-    console.log(req.params);
-    const {idcuadro} = req.params;
-    console.log(idcuadro);
-    const sql = "DELETE FROM cuadros WHERE idcuadros = ?";
-    db.query(sql,[idcuadro], (error, result) => {
-        console.log(result);
-        if(error){
-            return res.status(500).json({error : "ERROR: Intente mas tarde por favor"});
-        }
-        if(result.affectedRows == 0){
-            return res.status(404).send({error : "ERROR: El cuadro a borrar no existe"});
-        };
-        res.json({mesaje : "Cuadro Eliminado"});
-    }); 
-};
+  console.log(req.params);
+  const { idcuadro } = req.params;
+  console.log(idcuadro);
+  const sql = "DELETE FROM cuadros_v2 WHERE idcuadros_v2 = ?";
 
+  db.query(sql, [idcuadro], (error, result) => {
+    console.log(result);
+    if (error) {
+      return res
+        .status(500)
+        .json({ error: "ERROR: Intente mas tarde por favor" });
+    }
+    if (result.affectedRows == 0) {
+      return res
+        .status(404)
+        .send({ error: "ERROR: El cuadro a borrar no existe" });
+    }
+    res.json({ mesaje: "Cuadro Eliminado" });
+  });
+};
 
 // EXPORTAR DEL MODULO TODAS LAS FUNCIONES
 module.exports = {
-    allCuadros,
-    showCuadro,
-    storeCuadro,
-    updateCuadro,
-    destroyCuadro
+  allCuadros,
+  showCuadro,
+  storeCuadro,
+  updateCuadro,
+  destroyCuadro,
 };
