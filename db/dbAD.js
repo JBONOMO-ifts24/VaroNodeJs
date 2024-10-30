1fcb1bea94116cc44ec71c299f168da7f4dc2085
JBONOMO-ifts24 <29502079@ifts24.edu.ar>
2024-10-30 16:33:33 -0300

    Agregado de dbAD.js

---
+++ db/dbAD.js
@@ -0,0 +1,6 @@
+//Archivo para conectar a la base de datos de AllwaysData
+
+require('dotenv').config();
+const mysql = require('mysql2');
+const pool = mysql.createPool({ host: process.env.DB_HOST, user:process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_DATABASE,});                                                                                                                                   
+module.exports = pool; 
